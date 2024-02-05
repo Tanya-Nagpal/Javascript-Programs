604cec976a22ab59198115bd53b8bf4854214fcf
Tanya <121882110+Tanya-Nagpal@users.noreply.github.com>
2024-02-05 14:59:19 +0000

    operations

---
+++ 01_Basics/04_operations.js
@@ -0,0 +1,40 @@
+let value = 7
+let negValue = -value
+console.log(negValue);
+
+console.log(3+3);
+console.log(3-3);
+console.log(3*3);
+console.log(3**5);
+console.log(3/3);
+console.log(3%6);
+
+let str1 = "Hello"
+let str2 = "Tanya"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("3" + 5);
+// console.log(3 + "5");
+// console.log("3" + "5");
+// console.log("3" + 5 + 7);
+// console.log(3 + 5 + "7");
+
+// console.log((3 + 5) * 7 % 9);
+
+console.log(true);
+console.log(+true);
+// console.log(true+); //error
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 3+3
+
+let gameCounter = 100
+// gameCounter++;       //postfix
+++gameCounter;          //prefix
+console.log(gameCounter);
+
+//study link : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
