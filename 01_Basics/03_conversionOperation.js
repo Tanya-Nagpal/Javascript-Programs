629023f3b7f5986a9c944dca9d098fdb74c0066f
Tanya <121882110+Tanya-Nagpal@users.noreply.github.com>
2024-02-05 13:49:10 +0000

    conversion of datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "77"
+// let score = "77abc"
+// let score = undefined    
+// let score = null
+// let score = true
+
+/*
+console.log(typeof score)
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+*/
+
+// "77" => returns a number 
+// "77abc" => return NaN (Not a number)
+// undefined => returns undefined
+// null => return 0
+// boolean => return 1 if true and 0 if false
+
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true, 0 => false
+// "" => false
+// "abc" => true
