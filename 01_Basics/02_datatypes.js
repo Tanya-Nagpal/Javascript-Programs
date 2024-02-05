3452d3e2873750f1038aa1ee1ec6e252d55646bd
Tanya <121882110+Tanya-Nagpal@users.noreply.github.com>
2024-02-05 13:48:20 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict";       //treat allJS code as newer version
+
+// alert("Hello!")     //Can't use this, as we are using nodejs, not browser
+//Refer https://tc39.es/ for standards and https://developer.mozilla.org/en-US/docs/Web/javascript for documentation
+
+console.log(3+3)        //Maintain high code readability
+
+console.log("Tanya")
+
+let name = "Tanya"      //String
+let age = 21            //integer
+let isLoggedIn = false  //boolean
+let state;
+
+//number => 2^53
+//bigint
+//string => ""
+//null => standalone value (Representation of empty value)
+//undefined => No value for declared variable
+//symbol => unique
+
+//object
+
+console.log(typeof "Tanya");    //string
+console.log(typeof age)         //number
+console.log(typeof null);       //object
+console.log(typeof undefined);  //undefined
