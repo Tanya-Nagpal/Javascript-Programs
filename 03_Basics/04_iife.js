e74cfa318709d6251f9da309b3a88e5fdcfb6757
Tanya <121882110+Tanya-Nagpal@users.noreply.github.com>
2024-02-25 14:00:45 +0000

    Immediately invoked function expression

---
+++ 03_Basics/04_iife.js
@@ -0,0 +1,12 @@
+// IIFE: IMMEDIATELY INVOKED FUNCTION EXPRESSION
+// An IIFE is used to create a function expression that is immediately executed.
+
+// named IIFE
+(function code(){
+    console.log(`DB Connected`);
+})();       //use semi colon when you use two IIFE
+
+//unnamed IIFE with an arrow function
+((name) => {
+    console.log(`${name}, DB is connected!`);
+})('Tanya')     // Immediately invokes the arrow function with 'Tanya' as the argument
