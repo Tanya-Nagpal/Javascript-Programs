65ca80c2f9db8b7e8083150e4643a7227c4b68ef
Tanya <121882110+Tanya-Nagpal@users.noreply.github.com>
2024-03-09 12:01:12 +0000

    switch case example

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,61 @@
+// Switch case syntax
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("June");
+        break;
+    default:
+        console.log("Default case matched!");
+        break;
+}
+
+const monthStr = June
+
+switch (month) {
+    case "Jan":
+        console.log("January");
+        break;
+    case "Feb":
+        console.log("February");
+        break;
+    case "Mar":
+        console.log("March");
+        break;
+    case "Apr":
+        console.log("April");
+        break;
+    case "May":
+        console.log("May");
+        break;
+    case "June":
+        console.log("June");
+        break;
+    default:
+        console.log("Default case matched!");
+        break;
+}
